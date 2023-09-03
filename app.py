34b481e8c0d64db4c72442832e750af66f9243b6
amelie314 <qangela314@gmail.com>
2023-09-03 16:45:42 +0800

    every page now has 12 items

--- app.py
+++ app.py
@@ -1,100 +1,4 @@
-# from flask import Flask, render_template, jsonify
-# import mysql.connector
-
-# app = Flask(__name__)
-# app.config["JSON_AS_ASCII"] = False
-# app.config["TEMPLATES_AUTO_RELOAD"] = True
-
-# db_config = {
-#     'host': 'localhost',
-#     'user': 'root',
-#     'password': 'rootroot',
-#     'database': 'taipei_day_trip'
-# }
-
-# def fetch_images(cursor, attraction_id):
-#     cursor.execute("SELECT image_url FROM attraction_images WHERE attraction_id = %s", (attraction_id,))
-#     return [row['image_url'] for row in cursor.fetchall()]
-
-# # ...其他部分保持不變
-# def convert_to_dict(record):
-#     return {
-#         "id": record['id'],
-#         "name": record['name'],
-#         "category": record['CAT'],
-#         "description": record['description'],
-#         "address": record['address'],
-#         "transport": record['direction'],
-#         "mrt": record['MRT'],
-#         "lat": record['latitude'],
-#         "lng": record['longitude'],
-#         "images": []  # 我們稍後會填充這個部分
-#     }
-
-# @app.route("/")
-# def index():
-#     return render_template("index.html")
-
-# @app.route("/booking")
-# def booking():
-#     return render_template("booking.html")
-
-# @app.route("/thankyou")
-# def thankyou():
-#     return render_template("thankyou.html")
-
-# @app.route("/api/attractions", methods=['GET'])
-# def api_attractions():
-#     try:
-#         db = mysql.connector.connect(**db_config)
-#         cursor = db.cursor(dictionary=True)
-#         cursor.execute("SELECT * FROM attractions")
-#         attractions = cursor.fetchall()
-
-#         attractions_json = []
-#         for attraction in attractions:
-#             attraction_dict = convert_to_dict(attraction)
-#             attraction_dict['images'] = fetch_images(cursor, attraction['id'])
-#             attractions_json.append(attraction_dict)
-
-#     except mysql.connector.Error as err:
-#         print("Database error: {}".format(err))
-#         return jsonify({"error": "Database error"}), 500
-#     finally:
-#         cursor.close()
-#         db.close()
-
-#     output = {
-#         "nextPage": 1,
-#         "data": attractions_json
-#     }
-#     return jsonify(output)
-
-# @app.route("/api/attraction/<int:attractionId>", methods=['GET'])
-# def api_attraction(attractionId):
-#     try:
-#         db = mysql.connector.connect(**db_config)
-#         cursor = db.cursor(dictionary=True)
-#         cursor.execute("SELECT * FROM attractions WHERE id = %s", (attractionId,))
-#         attraction = cursor.fetchone()
-
-#         if attraction:
-#             attraction_dict = convert_to_dict(attraction)
-#             attraction_dict['images'] = fetch_images(cursor, attraction['id'])
-#             return jsonify(attraction_dict)
-#         else:
-#             return jsonify({"error": "Attraction not found"}), 404
-
-#     except mysql.connector.Error as err:
-#         print("Database error: {}".format(err))
-#         return jsonify({"error": "Database error"}), 500
-#     finally:
-#         cursor.close()
-#         db.close()
-
-# if __name__ == "__main__":
-#     app.run(host="0.0.0.0", port=3000)
-from flask import Flask, render_template, jsonify
+from flask import Flask, render_template, jsonify, request
 import mysql.connector
 import os
 from dotenv import load_dotenv
@@ -148,26 +52,68 @@ def booking():
 def thankyou():
     return render_template("thankyou.html")
 
+# @app.route("/api/attractions", methods=['GET'])
+# def api_attractions():
+#     try:
+#         db = mysql.connector.connect(**db_config)
+#         cursor = db.cursor(dictionary=True)
+#         cursor.execute("SELECT * FROM attractions")
+#         attractions = cursor.fetchall()
+#         attractions_json = [convert_to_dict(attraction, cursor) for attraction in attractions]
+        
+#         output = {
+#             "nextPage": 1,
+#             "data": attractions_json
+#         }
+#         return jsonify(output)
+#     except mysql.connector.Error as err:
+#         print("Database error: {}".format(err))
+#         return jsonify({"error": True, "message": "資料庫錯誤"}), 500
+#     finally:
+#         cursor.close()
+#         db.close()
 @app.route("/api/attractions", methods=['GET'])
 def api_attractions():
+    cursor = None  # 初始化為 None
+    db = None  # 初始化為 None
     try:
+        page = int(request.args.get('page', 0))
+        keyword = request.args.get('keyword')
+
         db = mysql.connector.connect(**db_config)
         cursor = db.cursor(dictionary=True)
-        cursor.execute("SELECT * FROM attractions")
+
+        sql_query = "SELECT * FROM attractions"
+        sql_params = []
+        
+        if keyword:
+            sql_query += " WHERE name LIKE %s"
+            sql_params.append("%" + keyword + "%")
+        
+        sql_query += " LIMIT %s, 12"
+        sql_params.append(page * 12)
+
+        cursor.execute(sql_query, tuple(sql_params))  # 將列表轉為元組
+
         attractions = cursor.fetchall()
         attractions_json = [convert_to_dict(attraction, cursor) for attraction in attractions]
-        
+
         output = {
-            "nextPage": 1,
+            "nextPage": page + 1 if attractions else None,
             "data": attractions_json
         }
+
         return jsonify(output)
+
     except mysql.connector.Error as err:
-        print("Database error: {}".format(err))
+        print("資料庫錯誤: {}".format(err))
         return jsonify({"error": True, "message": "資料庫錯誤"}), 500
     finally:
-        cursor.close()
-        db.close()
+        if cursor:  # 檢查 cursor 是否已賦值
+            cursor.close()
+        if db:  # 檢查 db 是否已賦值
+            db.close()
+
 
 @app.route("/api/attraction/<int:attractionId>", methods=['GET'])
 def api_attraction(attractionId):
@@ -197,7 +143,7 @@ def api_mrts():
         db = mysql.connector.connect(**db_config)
         cursor = db.cursor(dictionary=True)
 
-        # 取得數據庫库不重複的捷運站名稱
+        # 取得數據庫裡不重複的捷運站名稱，且
         cursor.execute("SELECT MRT, COUNT(*) as cnt FROM attractions GROUP BY MRT ORDER BY cnt DESC")
         mrt_names = [row['MRT'] for row in cursor.fetchall()]
 
