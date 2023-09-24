17b203e4ae00272bcdbd97a3c05aef2305679dde
amelie314 <qangela314@gmail.com>
2023-09-25 02:16:59 +0800

    - 拆解html     - base.html     - index.html     - attraction.html     - thankyou.html - 拆解css     - base.css     - index.css     - attraction.css - 拆解js     - base.js     - index.js     - attraction.js - 完成API串接     - 串接註冊功能     - 串接登入功能     - 串接查詢登入狀態功能 - 連接資料庫並創建會員table

--- app.py
+++ app.py
@@ -1,15 +1,26 @@
 from flask import Flask, render_template, jsonify, request
 import mysql.connector
 import os
+import re
 from dotenv import load_dotenv
+from werkzeug.security import generate_password_hash, check_password_hash
+from jwt import encode
+from jwt import decode
+import datetime
+from functools import wraps  #token驗證
+
 
 # 設定 Flask 應用
 app = Flask(__name__)
 app.config["JSON_AS_ASCII"] = False
 app.config["TEMPLATES_AUTO_RELOAD"] = True
 
+secret_key = os.environ.get("SECRET_KEY")
+app.config["SECRET_KEY"]= secret_key
+
 load_dotenv()
 password = os.environ.get("PASSWORD")
+
 
 # 資料庫連接設定
 db_config = {
@@ -38,8 +49,142 @@ def convert_to_dict(record, cursor):
         "lng": record['longitude'],
         "images": fetch_images(cursor, record['id'])  # 調用 fetch_images
     }
+    
+# 編碼 JWT 函式
+def encode_auth_token(user_id, username, email):
+    try:
+        payload = {
+            'exp': datetime.datetime.utcnow() + datetime.timedelta(days=7),
+            'iat': datetime.datetime.utcnow(),
+            'sub': {
+                'user_id': user_id,
+                'username': username,
+                'email': email
+            }
+        }
+        return encode(payload, app.config["SECRET_KEY"], algorithm='HS256')
+    except Exception as e:
+        return e
+    
+# 解碼 JWT 函式
+def decode_token(token):
+    try:
+        # 在這裡，'你的JWT密鑰' 應該和你用於編碼 JWT 的密鑰相同
+        decoded_data = decode(token, app.config["SECRET_KEY"], algorithms=['HS256'])
+        return decoded_data['sub'], None  # 回傳解碼後的資料和 None（代表沒有錯誤）
+    except Exception as e:
+        return None, str(e)  # 回傳 None 和錯誤訊息
 
-# 路由
+def token_required(f):
+    @wraps(f)
+    def decorated(*args, **kwargs):
+        
+        token = request.headers.get('Authorization')
+        # 去除 Bearer 但我剛才消除了
+        # token = auth_header.split(" ")[1] if auth_header else None
+
+        if not token:
+            return jsonify({'message': 'Token 缺失'}), 403
+        
+        try:
+            user_data, err = decode_token(token)  # 使用 decode_token 函數
+            if err:
+                raise Exception(err)
+        except Exception as e:
+            return jsonify({'message': f'Token 無效或過期: {str(e)}'}), 403
+        return f(user_data, *args, **kwargs)
+    return decorated
+
+# 註冊
+@app.route("/api/user", methods=["POST"])
+def register():
+    # 取得請求資料
+    data = request.json
+    name = data.get("name") 
+    email = data.get("email")
+    password = data.get("password")
+
+    # 驗證 email
+    if not email or not re.match(r"[^@]+@[^@]+\.[^@]+", email):
+        return jsonify({"error": True, "message": "無"}), 400
+    
+    # 驗證密碼長度
+    if not password or len(password) < 8:
+        return jsonify({"error": True, "message": "密碼必須至少有8個字元"}), 400
+
+    # 密碼加密
+    hashed_password = generate_password_hash(password, method='sha256')
+    
+    # 連接資料庫
+    db = mysql.connector.connect(**db_config)
+    cursor = db.cursor(dictionary=True)
+    
+    try:
+        # 檢查 Email 是否已存在
+        cursor.execute("SELECT * FROM users WHERE email = %s", (email,))
+        existing_user = cursor.fetchone()
+        
+        if existing_user:
+            return jsonify({"error": True, "message": "註冊失敗，Email 可能已被使用。"}), 400
+
+        # 新增使用者
+        cursor.execute("INSERT INTO users (name, email, password) VALUES (%s, %s, %s)", (name, email, hashed_password))
+        db.commit()
+        return jsonify({"ok": True}), 200
+    except mysql.connector.Error as err:
+        db.rollback()
+        return jsonify({"error": True, "message": str(err)}), 500
+    except Exception as err:
+        db.rollback()
+        return jsonify({"error": True, "message": "伺服器內部錯誤"}), 500
+    finally:
+        cursor.close()
+        db.close()
+
+# 登入
+@app.route("/api/user/auth", methods=['PUT'])
+def login():
+    # 取得請求資料
+    data = request.json
+    email = data.get('email')
+    password = data.get('password')
+
+    db = mysql.connector.connect(**db_config)
+    cursor = db.cursor(dictionary=True)
+
+    try:
+        cursor.execute("SELECT * FROM users WHERE email = %s", (email,))
+        user = cursor.fetchone()
+
+        if user and check_password_hash(user['password'], password):
+            
+            token = encode_auth_token(user['id'], user['name'], user['email'])
+            
+            return jsonify({'token': token}), 200
+        else:
+            return jsonify({'error': '登入失敗，Email 或密碼錯誤。'}), 400
+    except mysql.connector.Error as err:
+        print(f"資料庫錯誤：{err}")
+        return jsonify({'error': str(err)}), 500
+    except Exception as err:
+        print(f"伺服器錯誤：{err}")
+        return jsonify({'error': '伺服器錯誤'}), 500
+    finally:
+        cursor.close()
+        db.close()
+
+# 取得使用者資料        
+@app.route("/api/user/auth", methods=['GET'])
+@token_required  # 如果你有 token 驗證的裝飾器
+def get_user_auth(user_data=None):  # 更改名稱，user_data 從裝飾器中獲取
+    
+    print
+    
+    if not user_data:
+        return jsonify({"data": None}), 200
+    
+    return jsonify({"data": user_data}), 200
+ 
 @app.route("/")
 def index():
     return render_template("index.html")
